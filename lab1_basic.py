a919d53919be6289bfdf1b6d1bde4889c0e01940
Ian Hawke <I.Hawke@soton.ac.uk>
2016-01-31 20:04:50 +0000

    Generate website

--- lab1_basic.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr 22 09:12:34 2015
-
-@author: ih3
-"""
-
-import math
-x = math.sin(1.2)
--- lab1_import.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr 22 09:18:43 2015
-
-@author: ih3
-"""
-
-import lab1_basic
-print(lab1_basic.x)
