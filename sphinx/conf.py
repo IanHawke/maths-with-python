2c67152263d47ad63409a2ba919488a3b55fa060
Ian Hawke <I.Hawke@soton.ac.uk>
2016-03-09 14:49:59 +0000

    Change to pngmath so that epub builds.

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -30,7 +30,7 @@ import os
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.mathjax',
+    'sphinx.ext.pngmath',
     'nbsphinx',
 ]
 
--- sphinx/index.rst
+++ sphinx/index.rst
@@ -9,7 +9,7 @@ Welcome to Maths with Python's documentation!
 Contents:
 
 .. toctree::
-   :maxdepth: 2
+   :maxdepth: 1
 
    00-first-steps
    01-python-basics
